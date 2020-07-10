13be64f7798af9a0a67143de2c54f7b5010b3adf
prakumar62 <prakumar62@publicisgroupe.net>
2020-07-10 07:45:37 +0530

    First Rest request set up Maven,Test NG,Rest assured

---
+++ src/test/java/com/learning/api/RestDemo/FirstTestNGClass.java
@@ -0,0 +1,10 @@
+package com.learning.api.RestDemo;
+
+import org.testng.annotations.Test;
+
+public class FirstTestNGClass {
+  @Test
+  public void f() {
+	  System.out.println("Hello world");
+  }
+}
